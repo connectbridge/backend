24303252707d34c2ca934eaf42b6625ceb1bba9b
99corn <hahahoho5915@gmail.com>
2019-07-31 15:50:52 +0900

    Signed-off-by: 99corn <hahahoho5915@gmail.com>

--- src/main/java/com/bridge/MainController.java
+++ src/main/java/com/bridge/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 	@RequestMapping("/")
 	public String main() {
-		return "welcome";
+		return "welcome~~~";
 	}
 }
